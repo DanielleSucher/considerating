dd4cb8f69ac0e43af60befb1c0df1b61bc603a36
Danielle Sucher <dsucher@gmail.com>
2012-02-09 17:43:30 -0500

    Got google_oauth2 working

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
 		create! do |user|
 			user.provider = auth["provider"]
 			user.uid = auth["uid"]
-			user.name = auth["info"]["name"]
+			user.name = auth["info"]["first_name"]
 		end
 	end
 	
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, '496909690695.apps.googleusercontent.com', '784i1MUyj22KEmPsVtFdJLgY', {access_type: 'online', approval_prompt: '', :scope => 'https://www.googleapis.com/auth/userinfo.profile', :client_options => {:ssl => {:ca_path => "certs/cacert.pem"}}}
+    provider :google_oauth2, '496909690695.apps.googleusercontent.com', '784i1MUyj22KEmPsVtFdJLgY', {access_type: 'online', approval_prompt: '', :scope => 'https://www.googleapis.com/auth/userinfo.profile', client_options: {ssl: {ca_file: Rails.root.join('lib/assets/cacert.pem').to_s}}}
 end

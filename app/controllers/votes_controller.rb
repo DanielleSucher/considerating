005676a057d645674388339bc4e27534c3dae12d
Danielle Sucher <dsucher@gmail.com>
2012-02-23 16:31:30 -0500

    Fixed input/safarimobile bug & tweaked design

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,7 +4,11 @@ class VotesController < ApplicationController
 
   def create
     @consideration = Consideration.find(params[:vote][:voted_id])
-    current_user.vote!(@consideration, params[:vote][:rating])
+    if params[:vote][:rating] != nil
+	    current_user.vote!(@consideration, params[:vote][:rating])
+	else
+		current_user.vote!(@consideration, params[:rating])
+	end
     @consideration.reload
     respond_to do |format|
     	format.html { redirect_to consideration_path(@consideration) }
